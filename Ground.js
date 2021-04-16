793080607241fdb53c9b1098a971e8d758519c33
Aliza Ahmed <alizaahmed912007@gmail.com>
2021-04-16 17:36:18 +0530

    Third Commit

--- Ground.js
+++ Ground.js
@@ -13,7 +13,7 @@ class Ground
     display()
     {
       rectMode(CENTER);
-      fill("brown");
+      fill("yellow");
       rect(this.body.position.x, this.body.position.y, this.bodyW, this.bodyH);
     }
   };
